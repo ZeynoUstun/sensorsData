50d04fec7296cc461998b72a506ad42f5526a1fc
ZeynoUstun <zeynoustun@gmail.com>
2017-12-11 23:14:27 -0500

    Update finalSensor.js

--- finalSensor.js
+++ finalSensor.js
@@ -2,17 +2,17 @@ var request = require('request');
 const { Client } = require('pg');
 
 // PARTICLE PHOTON
-var device_id = '32002b001347353136383631';
-var access_token = 'e8ba164bb777f60ae0e9529a3bf809e7ebdf53c2';
+var device_id = ""process.env.PHOTON_ID"";
+var access_token = process.env.PHOTON_TOKEN;
 var particle_variable = 'workData';
 var device_url = 'https://api.particle.io/v1/devices/' + device_id + '/' + particle_variable + '?access_token=' + access_token;
 
 // AWS RDS POSTGRESQL INSTANCE
 var db_credentials = new Object();
 db_credentials.user = 'zeynoustun';
-db_credentials.host = 'labordata.cp4pyp6nced4.us-east-1.rds.amazonaws.com';
+db_credentials.host = process.env.AWSRDS_EP;
 db_credentials.database = 'laborhours';
-db_credentials.password = 'zyler2016';
+db_credentials.password = process.env.AWSRDS_PW;
 db_credentials.port = 5432;
 
 var getAndWriteData = function() {
@@ -40,4 +40,4 @@ var getAndWriteData = function() {
 };
 
 // write a new row of sensor data every five minutes
-setInterval(getAndWriteData, 60000);
+setInterval(getAndWriteData, 300000);
